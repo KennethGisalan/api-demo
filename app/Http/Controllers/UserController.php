1b0d6b4d0137aa651769d3bb1dd2731d9cc8cd66
Kenneth Gisalan <kgisalan@ondemand-innovation.com>
2025-05-23 13:37:08 +0800

    init

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+       $users = User::all();
+        return response()->json([
+            'status' => 'success',
+            'data' => $users
+        ]);
+    }
+
+    public function test(){
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Test route is working'
+        ]);
+    }
 }
